6e2167198706a6ff918e7e309b83760d9c3260c1
w0472826 <w0472826@nscc.ca>
2023-12-05 10:39:58 -0400

    Done

--- Form1.cs
+++ Form1.cs
@@ -9,7 +9,7 @@ namespace WinFormsMLEnterprise
     {
         private readonly MLContext mlContext;
         private IEstimator<ITransformer> dataPrepPipeline;
-        private ITransformer trainedModel;
+        private ITransformer? trainedModel;
 
         public Form1()
         {
@@ -21,26 +21,10 @@ namespace WinFormsMLEnterprise
             // Initialize data prep pipeline
             dataPrepPipeline = BuildDataPrepPipeline();
 
-            string modelFileName = "MLModel1.zip"; 
-            string modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, modelFileName);
-
-            // Load trained model with full pipeline
-            if (File.Exists(modelPath))
-            {
-                // Load trained model with full pipeline
-                trainedModel = mlContext.Model.Load(modelPath, out var modelSchema);
-                
-            }
-            else
-            {
-                MessageBox.Show ("Model file not found at:" + modelPath);
-            }
         }
 
         private void btnGuess_Click(object sender, EventArgs e)
         {
-            // Show a message box with the current directory
-            MessageBox.Show("Current Directory: " + Directory.GetCurrentDirectory());
 
             // Load sample data
             MLModel1.ModelInput sampleData = new MLModel1.ModelInput()
@@ -51,6 +35,7 @@ namespace WinFormsMLEnterprise
             // Load model and predict output
             var predictionResult = MLModel1.Predict(sampleData);
 
+            // Display predicted label and confidence score
             txtGuess.Text = predictionResult.PredictedLabel.ToString();
             txtConfidence.Text = (predictionResult.Score[0] * 100).ToString();
         }
@@ -58,6 +43,7 @@ namespace WinFormsMLEnterprise
 
         private IEstimator<ITransformer> BuildDataPrepPipeline()
         {
+            // Data preparation pipeline: (Map sentiment labels to keys, Feature the text, Map setiment key back to values)
             return mlContext.Transforms.Conversion.MapValueToKey("Label", "Sentiment")
                 .Append(mlContext.Transforms.Text.FeaturizeText("Features", "Comment"))
                 .Append(mlContext.Transforms.Conversion.MapKeyToValue("Label"));
@@ -65,44 +51,109 @@ namespace WinFormsMLEnterprise
 
         private void btnRetrain_Click(object sender, EventArgs e)
         {
-            // Create new data
-            Feedback feedbackNewData = new Feedback
-            {
-                Comment = txtReview.Text
-            };
+            btnRetrain_Click(sender, e, mlContext);
+        }
 
-            // Load new data into an IDataView
-            IDataView newData = mlContext.Data.LoadFromEnumerable(new[] { feedbackNewData });
+        private void btnRetrain_Click(object sender, EventArgs e, MLContext mlContext)
+        {
+
+            try
+            { // Set the working directory to the directory where the executable is located
+                Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+                // Load existing model or initialize a new one
+                if (trainedModel == null)
+                {
+                    trainedModel = BuildAndTrainModel(mlContext, dataPrepPipeline);
+                }
+
+                // Declare and initialize modelPath
+                string modelFileName = "MLModel1.zip";
+                string modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, modelFileName);
+
+                // Create new data
+                Feedback feedbackNewData = new Feedback
+                {
+                    Comment = txtReview.Text
+                };
+
+                // Load new data into an IDataView
+                IDataView newData = mlContext.Data.LoadFromEnumerable(new[] { feedbackNewData });
+
+                // Apply data prep transforms to new data
+                IDataView transformedNewData = dataPrepPipeline.Fit(newData).Transform(newData);
+
+                // Use binary classification trainer for retraining
+                var retrainedModel = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression().Fit(transformedNewData);
+
+                // Get predictions from the original model
+                var originalPredictions = trainedModel.Transform(transformedNewData);
+
+                // Get predictions from the retrained model
+                var retrainedPredictions = retrainedModel.Transform(transformedNewData);
+
+                // Extract weights from the original model
+                var originalWeights = originalPredictions.GetColumn<float>("Score").ToArray();
+
+                // Extract weights from the retrained model
+                var retrainedWeights = retrainedPredictions.GetColumn<float>("Score").ToArray();
+
+                // Display original weight
+                txtOriginalWeight.Text = originalWeights[0].ToString();
+
+                // Display retrained weight
+                txtReTrainedWeight.Text = retrainedWeights[0].ToString();
+
+                // Calculate and display weight differences
+                var weightDiff = originalWeights[0] - retrainedWeights[0];
+                txtWeightDiffs.Text = weightDiff.ToString();
+
+                // Load trained model with full pipeline
+                if (File.Exists(modelPath))
+                {
+                    // Load pre-existing trained model with full pipeline
+                    trainedModel = mlContext.Model.Load(modelPath, out var modelSchema);
+                }
+                else
+                {
+                    MessageBox.Show("Pre-existing model file not found at:" + modelPath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error in btnRetrain_Click: {ex.Message}");
+            }
+        }
+
+        private ITransformer BuildAndTrainModel(MLContext mlContext, IEstimator<ITransformer> dataPrepPipeline)
+        {
+            // Load or create new data
+            Feedback[] feedbackData = new Feedback[] { new Feedback { Comment = txtReview.Text } };
+            IDataView newData = mlContext.Data.LoadFromEnumerable(feedbackData);
 
             // Apply data prep transforms to new data
             IDataView transformedNewData = dataPrepPipeline.Fit(newData).Transform(newData);
 
-            // Use binary classification trainer for retraining
-            var retrainedModel = mlContext.BinaryClassification.Trainers.SdcaLogisticRegression().Fit(transformedNewData);
-
-            // Get predictions from the original model
-            //var originalPredictions = trainedModel.Transform(transformedNewData);
-
-            // Get predictions from the retrained model
-            var retrainedPredictions = retrainedModel.Transform(transformedNewData);
-
-            // Extract weights from the original model
-            //var originalWeights = originalPredictions.GetColumn<float>("Score").ToArray();
-
-            // Extract weights from the retrained model
-            var retrainedWeights = retrainedPredictions.GetColumn<float>("Score").ToArray();
-
-            // Display original weight
-            //txtOriginalWeight.Text = originalWeights[0].ToString();
-
-            // Display retrained weight
-            txtReTrainedWeight.Text = retrainedWeights[0].ToString();
-
-            // Calculate and display weight differences
-            //var weightDiff = originalWeights[0] - retrainedWeights[0];
-            //txtWeightDiffs.Text = weightDiff.ToString();
+            // Build and train the model
+            return BuildAndTrainModel(mlContext, transformedNewData);
         }
 
+        private ITransformer BuildAndTrainModel(MLContext mlContext, IDataView transformedData)
+        {
+            // Build the estimator pipeline for training
+            var estimator = mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: "Comment")
+                .Append(mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", maximumNumberOfIterations: 100));
+
+            // Fit the model to the transformed data
+            var model = estimator.Fit(transformedData);
+
+            // Save the trained model
+            string modelFileName = "MLModel1.zip";
+            string modelPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, modelFileName);
+            mlContext.Model.Save(model, transformedData.Schema, modelPath);
+
+            return model;
+        }
 
         public class Feedback
         {
